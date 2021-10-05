7330eb41974accda399d09093edb078c7049cebe
Jose Celano <josecelano@gmail.com>
2021-10-05 13:02:33 +0100

    [#35] Use real dvc-diff output from previous step

--- .github/actions/dvc-diff/entrypoint.sh
+++ .github/actions/dvc-diff/entrypoint.sh
@@ -11,6 +11,10 @@ pwd
 # https://github.com/iterative/dvc/issues/6720
 echo "dvc diff --show-json $PREVIOUS_REF $CURRENT_REF"
 DIFF=$(dvc diff --show-json $PREVIOUS_REF $CURRENT_REF)
+
+# Debug. Force output value
+#DIFF='{"added": [{"path": "data/000001/32/000001-32.600.2.tif"}], "deleted": [], "modified": [], "renamed": []}'
+
 echo $DIFF
 
 echo "::set-output name=diff::$DIFF"
