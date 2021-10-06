6df5a06c34da063f28f2bbd3d40e75ff42441663
Jose Celano <josecelano@gmail.com>
2021-10-06 10:12:44 +0100

    [#16] Linting

--- .github/actions/resize-image/src/resizing/resize.py
+++ .github/actions/resize-image/src/resizing/resize.py
@@ -1,15 +1,18 @@
 import pyvips
 
+
 def getImageFactor(image, output_size):
     width = image.width
     height = image.height
     factor_width = output_size[0] / width
     factor_height = output_size[1] / height
     return min(factor_height, factor_width)
-        
+
 
 def resize_image(source_image_path, resized_image_path, output_size):
     image = pyvips.Image.new_from_file(source_image_path, access='sequential')
-    result = image.resize(getImageFactor(image, output_size), kernel='lanczos2')
+    result = image.resize(getImageFactor(
+        image, output_size), kernel='lanczos2')
     result.write_to_file(resized_image_path)
-    print("Image",source_image_path,"resized to",result.width,"x",result.height, "as",resized_image_path)
+    print("Image", source_image_path, "resized to", result.width,
+          "x", result.height, "as", resized_image_path)
