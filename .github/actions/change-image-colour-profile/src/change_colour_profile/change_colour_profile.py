a6b1cf9ec379f17e5a807c2c074fd4238fac59c2
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2021-10-18 11:46:52 +0100

    Modify action to use dvc-diff-style inputs

--- .github/actions/change-image-colour-profile/src/change_colour_profile/change_colour_profile.py
+++ .github/actions/change-image-colour-profile/src/change_colour_profile/change_colour_profile.py
@@ -2,12 +2,12 @@ import pyvips
 import os
 
 
-def createOutputFolder(resized_image_path):
+def create_output_folder(resized_image_path):
     os.mkdir(os.path.dirname(resized_image_path))
 
 
 def change_colour_profile(source_image_path, destination_image_path, profile):
     image = pyvips.Image.new_from_file(source_image_path, access='sequential')
     result = image.icc_transform(profile)
-    createOutputFolder(destination_image_path)
+    create_output_folder(destination_image_path)
     result.write_to_file(destination_image_path)
--- .github/actions/change-image-colour-profile/src/main.py
+++ .github/actions/change-image-colour-profile/src/main.py
@@ -1,10 +1,30 @@
 import os
+import json
 
 from change_colour_profile.change_colour_profile import change_colour_profile
 
 
+def parse_dvc_diff_ouput(dvc_diff):
+    # It gets a plain string list with the added or modified files from the diff json.
+    # Input: {"added": [{"path": "data/000001/32/000001-32.600.2.tif"}], "deleted": [], "modified": [], "renamed": []}
+    # Ouput: ['data/000001/32/000001-32.600.2.tif']
+    # Code Review: parse_dvd_diff_ouput function should be moved to an independant action or dvc-diff action.
+
+    # parse json
+    data = json.loads(dvc_diff)
+
+    # only interesting in added or modified files
+    filenames = data['added'] + data['modified']
+
+    # parse path from all items
+    filenames = [path_object['path'] for path_object in filenames]
+
+    return filenames
+
+
 def main():
-    source_images_path = os.environ["INPUT_SOURCE_IMAGES"].split(',')
+    source_images_path = parse_dvc_diff_ouput(
+        os.environ["INPUT_SOURCE_IMAGES"])
     resized_images_path = os.environ["INPUT_DESTINATION_IMAGES"].split(',')
     profile = os.environ["INPUT_PROFILE"]
     for index, image_path in enumerate(source_images_path):
