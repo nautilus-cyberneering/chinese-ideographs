00a46717a1ad4c96f21503a2f79ef5ca4d94121c
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2021-10-18 14:46:51 +0100

    Fix unchanged method name

--- .github/actions/modify-image-icc-profile/src/modify_icc_profile/modify_icc_profile.py
+++ .github/actions/modify-image-icc-profile/src/modify_icc_profile/modify_icc_profile.py
@@ -6,7 +6,7 @@ def create_output_folder(resized_image_path):
     os.mkdir(os.path.dirname(resized_image_path))
 
 
-def change_colour_profile(source_image_path, destination_image_path, profile):
+def modify_icc_profile(source_image_path, destination_image_path, profile):
     image = pyvips.Image.new_from_file(source_image_path, access='sequential')
     result = image.icc_transform(profile)
     create_output_folder(destination_image_path)
