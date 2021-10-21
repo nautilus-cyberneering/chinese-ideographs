2bf560d0169125c3a6a5cf0acffc3a85941175c0
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2021-10-21 09:24:40 +0100

    Modify action to use JSON structures as input-output

--- .github/actions/resize-image/src/main.py
+++ .github/actions/resize-image/src/main.py
@@ -1,6 +1,26 @@
 import os
+import os.path
+import json
+import sys
 
 from resizing.resize import resize_image
+
+
+def process_input_json(input_json):
+    # It gets a JSON object with information about the files removed and added, and returns the same object
+    # with a new property with a list of the files to be modified
+    # Input: {"added": [{"path": "data/000001/32/000001-32.600.2.tif"}], "deleted": [], "modified": [], "renamed": []}
+    # Output: {"added": ..., ..., "icc profile modify input": ["data/000001/32/000001-32.600.2.tif"]}
+    # Code Review: parse_dvd_diff_ouput function should be moved to an independant action or dvc-diff action.
+
+    # parse json
+    data = json.loads(input_json)
+
+    # parse path from all items
+    data['resize input'] = [path_object['path']
+                            for path_object in data['added'] + data['modified']]
+
+    return data
 
 
 def parse_size_parametres():
@@ -9,14 +29,28 @@ def parse_size_parametres():
     return (width, height)
 
 
+def get_output_filename(input_filename):
+    splittedFilename = os.path.splitext(input_filename)
+    return splittedFilename[0]+"-resized"+splittedFilename[1]
+
+
 def main():
-    source_images_path = os.environ["INPUT_SOURCE_IMAGES"].split(',')
-    resized_images_path = os.environ["INPUT_DESTINATION_IMAGES"].split(',')
+
+    def process_file(filename):
+        output_filename = get_output_filename(filename)
+        print("Resizing ", filename, "to", output_size[0],
+              "x", output_size[1], "as", output_filename)
+        resize_image(filename, output_filename, output_size)
+        processed_files.append(output_filename)
+
+    processed_json = process_input_json(
+        os.environ["INPUT_DVC_DIFF"])
     output_size = parse_size_parametres()
-    for index, image_path in enumerate(source_images_path):
-        print("Resizing", image_path, "to", output_size[0],
-              "x", output_size[1], "as", resized_images_path[index])
-        resize_image(image_path, resized_images_path[index], output_size)
+    processed_files = []
+    for image_path in processed_json['resize input']:
+        process_file(image_path)
+    processed_json['output'] = processed_files
+    print("::set-output name=result::", json.dumps(processed_json))
 
 
 if __name__ == "__main__":
