d0926724e29269b0d229826592fc416e48b72d29
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2021-10-27 12:32:27 +0100

    Add user identification to commit

--- .github/actions/auto-commit-push/entrypoint.sh
+++ .github/actions/auto-commit-push/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-
 FILES=$(python $GITHUB_WORKSPACE/.github/actions/auto-commit-push/process-input.py "$FILENAMES")
 
+git config --global user.email "githubaction@nautilus-cyberneering.de"
+git config --global user.name "github action bot"
 git checkout $BRANCH
 git pull
 
