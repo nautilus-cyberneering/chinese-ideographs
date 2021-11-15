6a963dfd037bd61564911608506818c0515a6c44
Jose Celano <josecelano@gmail.com>
2021-11-15 16:49:50 +0000

    [#110] Get the list of modified Base images from git modified .dvc files

--- .github/actions/auto-commit-images/src/auto_commit/auto_commit.py
+++ .github/actions/auto-commit-images/src/auto_commit/auto_commit.py
@@ -71,7 +71,15 @@ def get_new_base_images(repo):
 
 def get_modified_base_images(repo):
     all_modified_files = get_all_modified_files(repo)
-    return filter_base_images(filter_media_library_files(all_modified_files))
+
+    # Git detects only the modification of the dvc file.
+    # For example: data/000001/42/000001-42.600.2.tif.dvc
+    # but we return the relative path of the modified image: data/000001/42/000001-42.600.2.tif
+    # Then, we have to remove the .dvc extentions if it exist.
+    all_modified_files_without_dvc_extension = [
+        file.removesuffix('.dvc') for file in all_modified_files]
+
+    return filter_base_images(filter_media_library_files(all_modified_files_without_dvc_extension))
 
 
 def get_deleted_base_images(repo):
@@ -79,7 +87,7 @@ def get_deleted_base_images(repo):
     return filter_base_images(filter_media_library_files(all_deleted_files))
 
 
-def add_or_modify_base_images_in_git(repository, repo_dir, repo_token, repo_base_image_paths, branch_ref, commit_prefix):
+def add_base_images_in_git(repository, repo_dir, repo_token, repo_base_image_paths, branch_ref):
     gh = github.Github(repo_token)
 
     remote_repo = gh.get_repo(repository)
@@ -104,7 +112,7 @@ def add_or_modify_base_images_in_git(repository, repo_dir, repo_token, repo_base
 
         # Commit message
         base_image_filename = os.path.basename(repo_base_image_path)
-        commit_message = f'{commit_prefix} {base_image_filename}'
+        commit_message = f'Add Base image: {base_image_filename}'
 
         branch = os.path.basename(branch_ref)
 
@@ -113,7 +121,7 @@ def add_or_modify_base_images_in_git(repository, repo_dir, repo_token, repo_base
         print("Repo dir: ", repo_dir)
         print("Base image path: ", base_image_path)
         print("Base image pointer path: ", base_image_pointer_path)
-        print("Base iamge gitignore path: ", base_image_gitignore_path)
+        print("Base image gitignore path: ", base_image_gitignore_path)
         print("Commit message: ", commit_message)
         print("Branch ref: ", branch_ref)
         print("Branch: ", branch)
@@ -131,6 +139,80 @@ def add_or_modify_base_images_in_git(repository, repo_dir, repo_token, repo_base
             path=repo_base_image_gitignore_path, mode='100644', type='blob', sha=blob2.sha)
 
         elements = [element1, element2]
+
+        head_sha = remote_repo.get_branch(branch).commit.sha
+        print("Branch head sha: ", head_sha)
+
+        base_tree = remote_repo.get_git_tree(sha=head_sha)
+        print("Base tree: ", base_tree)
+
+        tree = remote_repo.create_git_tree(elements, base_tree)
+        print("Tree: ", tree)
+
+        parent = remote_repo.get_git_commit(sha=head_sha)
+        print("Parent: ", parent)
+
+        commit = remote_repo.create_git_commit(commit_message, tree, [parent])
+        print("New commit: ", commit)
+
+        branch_refs = remote_repo.get_git_ref(f'heads/{branch}')
+        print("Banch refs: ", branch_refs)
+
+        branch_refs.edit(sha=commit.sha)
+        print("New branch ref: ", commit.sha)
+
+        commits.append(commit.sha)
+
+    return commits
+
+
+def update_base_images_in_git(repository, repo_dir, repo_token, repo_base_image_paths, branch_ref):
+    gh = github.Github(repo_token)
+
+    remote_repo = gh.get_repo(repository)
+
+    commits = []
+
+    for repo_base_image_path in repo_base_image_paths:
+
+        # Files we have to include in the commit:
+        # data/000001/42/000001-42.600.2.tif.dvc  (dvc pointer)
+        # In this case the .gitignore file should already exists.
+
+        # Relative paths
+        repo_base_image_pointer_path = repo_base_image_path + '.dvc'
+        repo_base_image_gitignore_path = os.path.dirname(
+            repo_base_image_path) + '/.gitignore'
+
+        # Absolute paths
+        base_image_path = f'{repo_dir}/{repo_base_image_path}'
+        base_image_pointer_path = f'{repo_dir}/{repo_base_image_pointer_path}'
+        base_image_gitignore_path = f'{repo_dir}/{repo_base_image_gitignore_path}'
+
+        # Commit message
+        base_image_filename = os.path.basename(repo_base_image_path)
+        commit_message = f'Update Base image: {base_image_filename}'
+
+        branch = os.path.basename(branch_ref)
+
+        # Debug info
+        print("Auto-commit to update Base image: ")
+        print("Repo dir: ", repo_dir)
+        print("Base image path: ", base_image_path)
+        print("Base image pointer path: ", base_image_pointer_path)
+        print("Base image gitignore path: ", base_image_gitignore_path)
+        print("Commit message: ", commit_message)
+        print("Branch ref: ", branch_ref)
+        print("Branch: ", branch)
+
+        # Create git tree elements
+
+        content1 = open(base_image_pointer_path, "r").read()
+        blob1 = remote_repo.create_git_blob(content1, "utf-8")
+        element1 = github.InputGitTreeElement(
+            path=repo_base_image_pointer_path, mode='100644', type='blob', sha=blob1.sha)
+
+        elements = [element1]
 
         head_sha = remote_repo.get_branch(branch).commit.sha
         print("Branch head sha: ", head_sha)
@@ -193,8 +275,8 @@ def commit_added_base_images(local_repo, repository, repo_dir, repo_token, branc
         dvc_add_and_push_image(repo_dir, repo_base_image)
 
     # git commit Base image: dvc pointer, and .gitignore
-    commits_for_added_images = add_or_modify_base_images_in_git(
-        repository, repo_dir, repo_token, added_repo_base_image_paths, branch, 'Add Base image')
+    commits_for_added_images = add_base_images_in_git(
+        repository, repo_dir, repo_token, added_repo_base_image_paths, branch)
 
     return commits_for_added_images
 
@@ -209,27 +291,10 @@ def commit_modified_base_images(local_repo, repository, repo_dir, repo_token, br
         dvc_add_and_push_image(repo_dir, repo_base_image)
 
     # git commit Base image: dvc pointer, and .gitignore
-    commits_for_modified_images = add_or_modify_base_images_in_git(
-        repository, repo_dir, repo_token, modified_repo_base_image_paths, branch, 'Update Base image')
+    commits_for_modified_images = update_base_images_in_git(
+        repository, repo_dir, repo_token, modified_repo_base_image_paths, branch)
 
     return commits_for_modified_images
-
-
-def commit_deleted_base_images(local_repo, repository, repo_dir, repo_token, branch):
-    # Deleted Base images
-    deleted_repo_base_image_paths = get_deleted_base_images(local_repo)
-    print("Deleted Base images: ", deleted_repo_base_image_paths)
-
-    # dvc remove
-    for repo_base_image in deleted_repo_base_image_paths:
-        # We have to use the dvc image pointer (.dvc) not the tiff image
-        dvc_delete_local_and_remote_image(repo_dir, repo_base_image + '.dvc')
-
-    # git commit Base image: dvc pointer, and .gitignore
-    commits_for_deleted_images = add_or_modify_base_images_in_git(
-        repository, repo_dir, repo_token, deleted_repo_base_image_paths, branch, 'Delete Base image')
-
-    return commits_for_deleted_images
 
 
 def auto_commit(repository, repo_dir, repo_token, branch):
@@ -246,8 +311,7 @@ def auto_commit(repository, repo_dir, repo_token, branch):
     commits_for_modified_images = commit_modified_base_images(
         local_repo, repository, repo_dir, repo_token, branch)
 
-    commits_for_deleted_images = commit_deleted_base_images(
-        local_repo, repository, repo_dir, repo_token, branch)
+    # TODO: delete Base image if Gold image was deleted.
 
     # TODO: process renamed files. dvc diff could return an output like:
     # {added: [], deleted: [], modified: [], renamed: [{path: {old: data/000004/32/000004-32.600.2.tif, new: data/000005/32/000005-32.600.2.tif}}]}
@@ -255,4 +319,4 @@ def auto_commit(repository, repo_dir, repo_token, branch):
     # If we are renaming a GOld image we have to rename the corresponding Base image.
     # If we are using a different artwork id we should warning that the image content is the same and make the workflow fail.
 
-    return commits_for_added_images + commits_for_modified_images + commits_for_deleted_images
+    return commits_for_added_images + commits_for_modified_images
