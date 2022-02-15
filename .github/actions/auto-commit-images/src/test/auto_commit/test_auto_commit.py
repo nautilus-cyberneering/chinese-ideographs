20eb189ab72bee74ced45177ddec27fe78b86a73
yeraydavidrodriguez <yeray.rodriguez@edataconsulting.de>
2022-02-15 13:05:11 +0000

    Remove all actions

--- .github/actions/auto-commit-images/src/test/auto_commit/test_auto_commit.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from auto_commit.auto_commit import auto_commit
-
-
-def test_auto_commit():
-    assert True
--- .github/actions/validate-gold-image-folder/src/validation/file_locator.py
+++
@@ -1,7 +0,0 @@
-import os
-import pathlib
-import sys
-
-
-def file_locator(filename):
-    return f'data/{filename.artwork_id}/{filename.purpose_code}'
